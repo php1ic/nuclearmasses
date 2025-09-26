c940a7de8ba323bd1883a78d66e5240a9752f3e9
Ian Cullen <ian.james.cullen@gmail.com>
2025-09-26 22:46:15 +0100

    Fix bug after moving to a match statement
    I misread the condition so had the default and 2020 cases the wrong way
    round.

--- nuclearmasses/ame_mass_file.py
+++ nuclearmasses/ame_mass_file.py
@@ -9,29 +9,6 @@ class AMEMassFile(Parse):
         super().__init__()
         match year:
             case 2020:
-                self.HEADER = 39
-                self.FOOTER = 0
-                self.START_Z = 11
-                self.END_Z = 14
-                self.START_A = 16
-                self.END_A = 19
-                self.START_ME = 29
-                self.END_ME = 41
-                self.START_DME = 42
-                self.END_DME = 53
-                self.START_BE_PER_A = 54
-                self.END_BE_PER_A = 64
-                self.START_DBE_PER_A = 65
-                self.END_DBE_PER_A = 72
-                self.START_BETA_DECAY_ENERGY = 76
-                self.END_BETA_DECAY_ENERGY = 86
-                self.START_DBETA_DECAY_ENERGY = 87
-                self.END_DBETA_DECAY_ENERGY = 95
-                self.START_MICRO_U = 100
-                self.END_MICRO_U = 112
-                self.START_MICRO_DU = 113
-                self.END_MICRO_DU = 125
-            case _:
                 self.HEADER = 36
                 self.FOOTER = 0
                 self.START_Z = 11
@@ -54,6 +31,29 @@ class AMEMassFile(Parse):
                 self.END_MICRO_U = 120
                 self.START_MICRO_DU = 124
                 self.END_MICRO_DU = 135
+            case _:
+                self.HEADER = 39
+                self.FOOTER = 0
+                self.START_Z = 11
+                self.END_Z = 14
+                self.START_A = 16
+                self.END_A = 19
+                self.START_ME = 29
+                self.END_ME = 41
+                self.START_DME = 42
+                self.END_DME = 53
+                self.START_BE_PER_A = 54
+                self.END_BE_PER_A = 64
+                self.START_DBE_PER_A = 65
+                self.END_DBE_PER_A = 72
+                self.START_BETA_DECAY_ENERGY = 76
+                self.END_BETA_DECAY_ENERGY = 86
+                self.START_DBETA_DECAY_ENERGY = 87
+                self.END_DBETA_DECAY_ENERGY = 95
+                self.START_MICRO_U = 100
+                self.END_MICRO_U = 112
+                self.START_MICRO_DU = 113
+                self.END_MICRO_DU = 125
 
         self.column_limits = [
                 (self.START_Z, self.END_Z),
