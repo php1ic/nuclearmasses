69d62d10012b68ac82f94dd9001250e604a37a96
Ian Cullen <ian.james.cullen@gmail.com>
2025-09-26 21:53:49 +0100

    Update AME parser to use read_fwf
    The format of the file is given in the header of the file. It's wrong,
    but while finding that out I found that we can setup various lists and
    dictionaries to then use read_fwf and parse the file in one go. We still
    need to make a few adjustments once the parse is done, but they are
    fairly minor.

--- nuclearmasses/ame_mass_file.py
+++ nuclearmasses/ame_mass_file.py
@@ -6,51 +6,64 @@ class AMEMassFile(Parse):
     """Easy access to where the variables are in the AME mass file."""
 
     def __init__(self, year: int):
-        """Setup up the values."""
         super().__init__()
-        if year < 2020:
-            self.HEADER = 39
-            self.FOOTER = None
-            self.START_A = 16
-            self.END_A = 19
-            self.START_Z = 11
-            self.END_Z = 14
-            self.START_ME = 29
-            self.END_ME = 41
-            self.START_DME = 42
-            self.END_DME = 53
-            self.START_BE_PER_A = 54
-            self.END_BE_PER_A = 64
-            self.START_DBE_PER_A = 65
-            self.END_DBE_PER_A = 72
-            self.START_BETA_DECAY_ENERGY = 76
-            self.END_BETA_DECAY_ENERGY = 86
-            self.START_DBETA_DECAY_ENERGY = 87
-            self.END_DBETA_DECAY_ENERGY = 95
-            self.START_MICRO_U = 100
-            self.END_MICRO_U = 112
-            self.START_MICRO_DU = 113
-            self.END_MICRO_DU = 125
-        else:
-            self.HEADER = 36
-            self.FOOTER = None
-            self.START_A = 16
-            self.END_A = 19
-            self.START_Z = 11
-            self.END_Z = 14
-            self.START_ME = 29
-            self.END_ME = 42
-            self.START_DME = 43
-            self.END_DME = 53
-            self.START_BE_PER_A = 56
-            self.END_BE_PER_A = 66
-            self.START_DBE_PER_A = 69
-            self.END_DBE_PER_A = 77
-            self.START_BETA_DECAY_ENERGY = 82
-            self.END_BETA_DECAY_ENERGY = 93
-            self.START_DBETA_DECAY_ENERGY = 95
-            self.END_DBETA_DECAY_ENERGY = 104
-            self.START_MICRO_U = 110
-            self.END_MICRO_U = 120
-            self.START_MICRO_DU = 124
-            self.END_MICRO_DU = 135
+        match year:
+            case 2020:
+                self.HEADER = 39
+                self.FOOTER = 0
+                self.START_Z = 11
+                self.END_Z = 14
+                self.START_A = 16
+                self.END_A = 19
+                self.START_ME = 29
+                self.END_ME = 41
+                self.START_DME = 42
+                self.END_DME = 53
+                self.START_BE_PER_A = 54
+                self.END_BE_PER_A = 64
+                self.START_DBE_PER_A = 65
+                self.END_DBE_PER_A = 72
+                self.START_BETA_DECAY_ENERGY = 76
+                self.END_BETA_DECAY_ENERGY = 86
+                self.START_DBETA_DECAY_ENERGY = 87
+                self.END_DBETA_DECAY_ENERGY = 95
+                self.START_MICRO_U = 100
+                self.END_MICRO_U = 112
+                self.START_MICRO_DU = 113
+                self.END_MICRO_DU = 125
+            case _:
+                self.HEADER = 36
+                self.FOOTER = 0
+                self.START_Z = 11
+                self.END_Z = 14
+                self.START_A = 16
+                self.END_A = 19
+                self.START_ME = 29
+                self.END_ME = 42
+                self.START_DME = 43
+                self.END_DME = 53
+                self.START_BE_PER_A = 56
+                self.END_BE_PER_A = 66
+                self.START_DBE_PER_A = 69
+                self.END_DBE_PER_A = 77
+                self.START_BETA_DECAY_ENERGY = 82
+                self.END_BETA_DECAY_ENERGY = 93
+                self.START_DBETA_DECAY_ENERGY = 95
+                self.END_DBETA_DECAY_ENERGY = 104
+                self.START_MICRO_U = 110
+                self.END_MICRO_U = 120
+                self.START_MICRO_DU = 124
+                self.END_MICRO_DU = 135
+
+        self.column_limits = [
+                (self.START_Z, self.END_Z),
+                (self.START_A, self.END_A),
+                (self.START_ME, self.END_ME),
+                (self.START_DME, self.END_DME),
+                (self.START_BE_PER_A, self.END_BE_PER_A),
+                (self.START_DBE_PER_A, self.END_DBE_PER_A),
+                (self.START_BETA_DECAY_ENERGY, self.END_BETA_DECAY_ENERGY),
+                (self.START_DBETA_DECAY_ENERGY, self.END_DBETA_DECAY_ENERGY),
+                (self.START_MICRO_U, self.END_MICRO_U),
+                (self.START_MICRO_DU, self.END_MICRO_DU),
+                ]
--- nuclearmasses/ame_mass_parse.py
+++ nuclearmasses/ame_mass_parse.py
@@ -10,46 +10,93 @@ from nuclearmasses.ame_mass_file import AMEMassFile
 class AMEMassParser(AMEMassFile):
     """Parse the AME mass file.
 
-    The format is known but I don't think python can easily parse it.
+    The format is known but the provided string does not match all lines.
+    We will therefore use START and END markers, which are inherited, and
+    read the columns are interested in.
     """
 
     def __init__(self, filename: pathlib.Path, year: int):
-        """Set the file to read and table year."""
-        self.filename = filename
-        self.year = year
+        """Set the file to read and table year"""
+        self.filename: pathlib.Path = filename
+        self.year: int = year
         super().__init__(self.year)
         logging.info(f"Reading {self.filename} from {self.year}")
 
-    def _read_line(self, line: str) -> dict:
-        """Read a line from the file."""
-        if line.find("#") != -1:
-            line = line.replace("#", " ")
+    def _column_names(self) -> list[str]:
+        """Set the column name depending on the year"""
+        match self.year:
+            case 2020:
+                return [
+                        "Z",
+                        "A",
+                        "MassExcess",
+                        "MassExcessError",
+                        "BindingEnergyPerA",
+                        "BindingEnergyPerAError",
+                        "BetaDecayEnergy",
+                        "BetaDecayEnergyError",
+                        "AtomicMass",
+                        "AtomicMassError"
+                        ]
 
-        data = {
-            "TableYear": self.year,
-            "A": self._read_as_int(line, self.START_A, self.END_A),
-            "Z": self._read_as_int(line, self.START_Z, self.END_Z),
-            "AMEMassExcess": self._read_as_float(line, self.START_ME, self.END_ME),
-            "AMEMassExcessError": self._read_as_float(line, self.START_DME, self.END_DME),
-            "BindingEnergyPerA": self._read_as_float(line, self.START_BE_PER_A, self.END_BE_PER_A),
-            "BindingEnergyPerAError": self._read_as_float(line, self.START_DBE_PER_A, self.END_DBE_PER_A),
-            "BetaDecayEnergy": self._read_as_float(line, self.START_BETA_DECAY_ENERGY, self.END_BETA_DECAY_ENERGY),
-            "BetaDecayEnergyError": self._read_as_float(line, self.START_DBETA_DECAY_ENERGY, self.END_DBETA_DECAY_ENERGY),
-            "AtomicMass": self._read_as_float(line, self.START_MICRO_U, self.END_MICRO_U),
-            "AtomicMassError": self._read_as_float(line, self.START_MICRO_DU, self.END_MICRO_DU),
-        }
+    def _data_types(self) -> dict:
+        """Set the data type depending on the year"""
+        match self.year:
+            case 2020:
+                return {
+                        "Symbol": "string",
+                        "N": "Int64",
+                        "Z": "Int64",
+                        "A": "Int64",
+                        "MassExcess": "float64",
+                        "MassExcessError": "float64",
+                        "BindingEnergyPerA": "float64",
+                        "BindingEnergyPerAError": "float64",
+                        "BetaDecayEnergy": "float64",
+                        "BetaDecayEnergyError": "float64",
+                        "AtomicMass": "float64",
+                        "AtomicMassError": "float64",
+                        }
 
-        data["N"] = data["A"] - data["Z"]
-        data["Symbol"] = self.z_to_symbol[data["Z"]]
-
-        return data
+    def _na_values(self) -> dict:
+        """Set the columns that have placeholder values"""
+        match self.year:
+            case 2020:
+                return {
+                        "BetaDecayEnergy": ['', '*'],
+                        "BetaDecayEnergyError": ['', '*'],
+                        }
 
     def read_file(self) -> pd.DataFrame:
-        """Read the file."""
-        with open(self.filename, "r") as f:
-            lines = [line.rstrip() for line in f]
+        """Read the file using it's known format
 
-        # Remove the header lines
-        lines = lines[self.HEADER: self.FOOTER]
+        The AMEMassFile and other functions in this class have hopefully sanitized the
+        column names, data types and locations of the date so we can not make the generic
+        call to parse the file.
+        """
+        try:
+            df = pd.read_fwf(
+                    self.filename,
+                    colspecs=self.column_limits,
+                    names=self._column_names(),
+                    na_values=self._na_values(),
+                    keep_default_na=False,
+                    on_bad_lines='warn',
+                    skiprows=self.HEADER,
+                    skipfooter=self.FOOTER
+                    )
+            df["N"] = df["A"] - df["Z"]
+            df["Symbol"] = df["Z"].map(self.z_to_symbol)
 
-        return pd.DataFrame([self._read_line(line) for line in lines])
+            # We use the NUBASE data to define whether or not an isotope is experimentally measured,
+            # so for this data we'll just drop any and all '#' characters
+            df.replace("#", "", regex=True, inplace=True)
+
+            # We skipped the reading of the initial part of the atomic mass as it's always the same as A.
+            # Add it back on here and remove the now redundant decimal point from the other part of the number.
+            df["AtomicMass"] = df["A"].astype(str) + "." + df["AtomicMass"].str.replace(".", "", regex=False)
+
+            return df.astype(self._data_types())
+        except ValueError as e:
+            print(f"Parsing error: {e}")
+
