f85f34b0b8f4c68e2ffa8ba102a4bb50627888ac
Ian Cullen <ian.james.cullen@gmail.com>
2025-09-26 22:47:23 +0100

    Convert AME reaction1 to read_fwf parsing

--- nuclearmasses/ame_reaction_1_parse.py
+++ nuclearmasses/ame_reaction_1_parse.py
@@ -20,42 +20,95 @@ class AMEReactionParserOne(AMEReactionFileOne):
         super().__init__(self.year)
         logging.info(f"Reading {self.filename} from {self.year}")
 
-    def _read_line(self, line: str) -> dict:
-        """Read a line from the file."""
-        # Don't use a '#' as an experimental marker in this file
-        # but still need to remove it
-        if line.find("#") != -1:
-            line = line.replace("#", " ")
+    def _column_names(self) -> list[str]:
+        """Set the column name depending on the year"""
+        match self.year:
+            case 2020:
+                return [
+                        "A",
+                        "Z",
+                        "TwoNeutronSeparationEnergy",
+                        "TwoNeutronSeparationEnergyError",
+                        "TwoProtonSeparationEnergy",
+                        "TwoProtonSeparationEnergyError",
+                        "QAlpha",
+                        "QAlphaError",
+                        "QTwoBeta",
+                        "QTwoBetaError",
+                        "QEpsilon",
+                        "QEpsilonError",
+                        "QBetaNeutron",
+                        "QBetaNeutronError",
+                        ]
 
-        data = {
-            "TableYear": self.year,
-            "A": self._read_as_int(line, self.START_R1_A, self.END_R1_A),
-            "Z": self._read_as_int(line, self.START_R1_Z, self.END_R1_Z),
-            "TwoNeutronSeparationEnergy": self._read_as_float(line, self.START_S2N, self.END_S2N),
-            "TwoNeutronSeparationEnergyError": self._read_as_float(line, self.START_DS2N, self.END_DS2N),
-            "TwoProtonSeparationEnergy": self._read_as_float(line, self.START_S2P, self.END_S2P),
-            "TwoProtonSeparationEnergyError": self._read_as_float(line, self.START_DS2P, self.END_DS2P),
-            "QAlpha": self._read_as_float(line, self.START_QA, self.END_QA),
-            "QAlphaError": self._read_as_float(line, self.START_DQA, self.END_DQA),
-            "QTwoBeta": self._read_as_float(line, self.START_Q2B, self.END_Q2B),
-            "QTwoBetaError": self._read_as_float(line, self.START_DQ2B, self.END_DQ2B),
-            "QEpsilon": self._read_as_float(line, self.START_QEP, self.END_QEP),
-            "QEpsilonError": self._read_as_float(line, self.START_DQEP, self.END_DQEP),
-            "QBetaNeutron": self._read_as_float(line, self.START_QBN, self.END_QBN),
-            "QBetaNeutronError": self._read_as_float(line, self.START_DQBN, self.END_DQBN),
-        }
+    def _data_types(self) -> dict:
+        """Set the data type depending on the year"""
+        match self.year:
+            case 2020:
+                return {
+                        "Symbol": "string",
+                        "A": "Int64",
+                        "Z": "Int64",
+                        "N": "Int64",
+                        "TwoNeutronSeparationEnergy": "float64",
+                        "TwoNeutronSeparationEnergyError": "float64",
+                        "TwoProtonSeparationEnergy": "float64",
+                        "TwoProtonSeparationEnergyError": "float64",
+                        "QAlpha": "float64",
+                        "QAlphaError": "float64",
+                        "QTwoBeta": "float64",
+                        "QTwoBetaError": "float64",
+                        "QEpsilon": "float64",
+                        "QEpsilonError": "float64",
+                        "QBetaNeutron": "float64",
+                        "QBetaNeutronError": "float64",
+                        }
 
-        data["N"] = data["A"] - data["Z"]
-        data["Symbol"] = self.z_to_symbol[data["Z"]]
-
-        return data
+    def _na_values(self) -> dict:
+        """Set the columns that have placeholder values"""
+        match self.year:
+            case 2020:
+                return {
+                        "TwoNeutronSeparationEnergy": ['', '*'],
+                        "TwoNeutronSeparationEnergyError": ['', '*'],
+                        "TwoProtonSeparationEnergy": ['', '*'],
+                        "TwoProtonSeparationEnergyError": ['', '*'],
+                        "QAlpha": ['', '*'],
+                        "QAlphaError": ['', '*'],
+                        "QTwoBeta": ['', '*'],
+                        "QTwoBetaError": ['', '*'],
+                        "QEpsilon": ['', '*'],
+                        "QEpsilonError": ['', '*'],
+                        "QBetaNeutron": ['', '*'],
+                        "QBetaNeutronError": ['', '*'],
+                        }
 
     def read_file(self) -> pd.DataFrame:
-        """Read the file."""
-        with open(self.filename, "r") as f:
-            lines = [line.rstrip() for line in f]
+        """Read the file using it's known format
 
-        # Remove the header lines
-        lines = lines[self.HEADER: self.FOOTER]
+        The AMEReactionFileOne and other functions in this class have hopefully sanitized the
+        column names, data types and locations of the date so we can not make the generic
+        call to parse the file.
+        """
+        try:
+            df = pd.read_fwf(
+                    self.filename,
+                    colspecs=self.column_limits,
+                    names=self._column_names(),
+                    na_values=self._na_values(),
+                    keep_default_na=False,
+                    on_bad_lines='warn',
+                    skiprows=self.HEADER,
+                    skipfooter=self.FOOTER
+                    )
+            df["N"] = df["A"] - df["Z"]
+            df["Symbol"] = df["Z"].map(self.z_to_symbol)
 
-        return pd.DataFrame([self._read_line(line) for line in lines])
+            # We use the NUBASE data to define whether or not an isotope is experimentally measured,
+            # so for this data we'll just drop any and all '#' characters
+            df.replace("#", "", regex=True, inplace=True)
+
+            return df.astype(self._data_types())
+        except ValueError as e:
+            print(f"Parsing error: {e}")
+
