2b3962c25f4f6f909fb8722852bd1364c8ef3497
Ian Cullen <ian.james.cullen@gmail.com>
2025-09-28 22:05:16 +0100

    Update NUBASE parsing and tests
    There is a lot going on with the line format and the format within a
    column so for the moment, it's quite a rough parse and we drop a few
    columns so there is scope for future improvements.

--- nuclearmasses/nubase_file.py
+++ nuclearmasses/nubase_file.py
@@ -14,72 +14,137 @@ class NUBASEFile(Parse):
     def __init__(self, year: int):
         """Setup the values that locate the variable."""
         super().__init__()
-        if year < 2020:
-            self.HEADER = 0
-            self.FOOTER = None
-            self.START_A = 0
-            self.END_A = 3
-            self.START_Z = 4
-            self.END_Z = 7
-            self.START_STATE = 7
-            self.END_STATE = 8
-            self.START_ME = 18
-            self.END_ME = 29
-            self.START_DME = 29
-            self.END_DME = 38
-            self.START_ISOMER = 39
-            self.END_ISOMER = 46
-            self.START_DISOMER = 48
-            self.END_DISOMER = 56
-            self.START_HALFLIFEVALUE = 60
-            self.END_HALFLIFEVALUE = 68
-            self.START_HALFLIFEUNIT = 69
-            self.END_HALFLIFEUNIT = 71
-            self.START_HALFLIFEERROR = 72
-            self.END_HALFLIFEERROR = 77
-            self.START_SPIN = 79
-            self.END_SPIN = 93
-            # After the 2003 table the discovery
-            # year was added alterting the positions
-            self.START_YEAR = 105
-            self.END_YEAR = 109
-            # Let the 03 position be the odd-one-out and thus
-            # have the slightly awkward name
-            self.START_DECAYSTRING_03 = 106
-            self.START_DECAYSTRING = 110
-            # The decay string goes to EOL put here commented
-            # to show that we haven't just forgotten about it.
-            # END_DECAYSTRING = EOL;
-        else:
-            self.HEADER = 25
-            self.FOOTER = None
-            self.START_A = 0
-            self.END_A = 3
-            self.START_Z = 4
-            self.END_Z = 7
-            self.START_STATE = 7
-            self.END_STATE = 8
-            self.START_ME = 18
-            self.END_ME = 30
-            self.START_DME = 31
-            self.END_DME = 41
-            self.START_ISOMER = 42
-            self.END_ISOMER = 53
-            self.START_DISOMER = 54
-            self.END_DISOMER = 64
-            self.START_HALFLIFEVALUE = 69
-            self.END_HALFLIFEVALUE = 77
-            self.START_HALFLIFEUNIT = 78
-            self.END_HALFLIFEUNIT = 80
-            self.START_HALFLIFEERROR = 81
-            self.END_HALFLIFEERROR = 87
-            self.START_SPIN = 88
-            self.END_SPIN = 101
-            self.START_ENSDF = 102
-            self.END_ENSDF = 103
-            self.START_YEAR = 114
-            self.END_YEAR = 118
-            self.START_DECAYSTRING = 119
-            # The decay string goes to EOL put here commented
-            # to show that we haven't just forgotten about it.
-            # self.END_DECAYSTRING = EOL;
+        match year:
+            case 1995:
+                self.HEADER = 0
+                self.FOOTER = 0
+                self.START_A = 0
+                self.END_A = 3
+                self.START_Z = 4
+                self.END_Z = 7
+                self.START_STATE = 7
+                self.END_STATE = 8
+                self.START_ME = 18
+                self.END_ME = 29
+                self.START_DME = 29
+                self.END_DME = 38
+                self.START_ISOMER = 39
+                self.END_ISOMER = 46
+                self.START_DISOMER = 48
+                self.END_DISOMER = 56
+                self.START_HALFLIFEVALUE = 60
+                self.END_HALFLIFEVALUE = 68
+                self.START_HALFLIFEUNIT = 69
+                self.END_HALFLIFEUNIT = 71
+                self.START_HALFLIFEERROR = 72
+                self.END_HALFLIFEERROR = 77
+                self.START_SPIN = 79
+                self.END_SPIN = 93
+                self.START_DECAYSTRING = 106
+                self.END_DECAYSTRING = None
+            case 2003:
+                self.HEADER = 0
+                self.FOOTER = 0
+                self.START_A = 0
+                self.END_A = 3
+                self.START_Z = 4
+                self.END_Z = 7
+                self.START_STATE = 7
+                self.END_STATE = 8
+                self.START_ME = 18
+                self.END_ME = 29
+                self.START_DME = 29
+                self.END_DME = 38
+                self.START_ISOMER = 39
+                self.END_ISOMER = 46
+                self.START_DISOMER = 48
+                self.END_DISOMER = 56
+                self.START_HALFLIFEVALUE = 60
+                self.END_HALFLIFEVALUE = 68
+                self.START_HALFLIFEUNIT = 69
+                self.END_HALFLIFEUNIT = 71
+                self.START_HALFLIFEERROR = 72
+                self.END_HALFLIFEERROR = 77
+                self.START_SPIN = 79
+                self.END_SPIN = 93
+                self.START_DECAYSTRING = 106
+                self.END_DECAYSTRING = None
+            case 2020:
+                self.HEADER = 25
+                self.FOOTER = 0
+                self.START_A = 0
+                self.END_A = 3
+                self.START_Z = 4
+                self.END_Z = 7
+                self.START_STATE = 7
+                self.END_STATE = 8
+                self.START_ME = 18
+                self.END_ME = 31
+                self.START_DME = 31
+                self.END_DME = 42
+                self.START_ISOMER = 43
+                self.END_ISOMER = 53
+                self.START_DISOMER = 54
+                self.END_DISOMER = 64
+                self.START_HALFLIFEVALUE = 69
+                self.END_HALFLIFEVALUE = 77
+                self.START_HALFLIFEUNIT = 78
+                self.END_HALFLIFEUNIT = 80
+                self.START_HALFLIFEERROR = 81
+                self.END_HALFLIFEERROR = 87
+                self.START_SPIN = 88
+                self.END_SPIN = 101
+                self.START_ENSDF = 102
+                self.END_ENSDF = 103
+                self.START_YEAR = 114
+                self.END_YEAR = 118
+                self.START_DECAYSTRING = 119
+                self.END_DECAYSTRING = None
+            case _:
+                self.HEADER = 0
+                self.FOOTER = 0
+                self.START_A = 0
+                self.END_A = 3
+                self.START_Z = 4
+                self.END_Z = 7
+                self.START_STATE = 7
+                self.END_STATE = 8
+                self.START_ME = 18
+                self.END_ME = 29
+                self.START_DME = 29
+                self.END_DME = 38
+                self.START_ISOMER = 39
+                self.END_ISOMER = 46
+                self.START_DISOMER = 48
+                self.END_DISOMER = 56
+                self.START_HALFLIFEVALUE = 60
+                self.END_HALFLIFEVALUE = 68
+                self.START_HALFLIFEUNIT = 69
+                self.END_HALFLIFEUNIT = 71
+                self.START_HALFLIFEERROR = 72
+                self.END_HALFLIFEERROR = 77
+                self.START_SPIN = 79
+                self.END_SPIN = 93
+                self.START_YEAR = 105
+                self.END_YEAR = 109
+                self.START_DECAYSTRING = 110
+                self.END_DECAYSTRING = None
+
+        self.column_limits = [
+                (self.START_A, self.END_A),
+                (self.START_Z, self.END_Z),
+                (self.START_STATE, self.END_STATE),
+                (self.START_ME, self.END_ME),
+                (self.START_DME, self.END_DME),
+                (self.START_ISOMER, self.END_ISOMER),
+                (self.START_DISOMER, self.END_DISOMER),
+                (self.START_HALFLIFEVALUE, self.END_HALFLIFEVALUE),
+                (self.START_HALFLIFEUNIT, self.END_HALFLIFEUNIT),
+                (self.START_HALFLIFEERROR, self.END_HALFLIFEERROR),
+                (self.START_SPIN, self.END_SPIN),
+                (self.START_DECAYSTRING, self.END_DECAYSTRING),
+                ]
+
+        if year > 2003:
+            print("In here")
+            self.column_limits.insert(-1, (self.START_YEAR, self.END_YEAR))
--- nuclearmasses/nubase_parse.py
+++ nuclearmasses/nubase_parse.py
@@ -1,8 +1,6 @@
-"""Extract the data from the nubse file."""
+"""Extract the data from the nubase file."""
 import logging
 import pathlib
-import re
-import typing
 
 import pandas as pd
 
@@ -22,116 +20,144 @@ class NUBASEParser(NUBASEFile):
         super().__init__(self.year)
         logging.info(f"Reading {self.filename} from {self.year}")
 
-    def _read_halflife_value(self, line: str) -> typing.Optional[float]:
-        """Slice the string to get the numerical value or None if it's empty."""
-        data = line[self.START_HALFLIFEVALUE: self.END_HALFLIFEVALUE].strip()
-        number = re.sub(r"[<>?~]", "", data)
-        return float(number) if number else None
+    def _column_names(self) -> list[str]:
+        """Set the column name depending on the year"""
+        match self.year:
+            case 1995 | 2003:
+                return [
+                        "A",
+                        "Z",
+                        "State",
+                        "NUBASEMassExcess",
+                        "NUBASEMassExcessError",
+                        "IsomerEnergy",
+                        "IsomerEnergyError",
+                        "HalfLifeValue",
+                        "HalfLifeUnit",
+                        "HalfLifeError",
+                        "Spin",
+                        "DecayModes",
+                        ]
+            case _:
+                return [
+                        "A",
+                        "Z",
+                        "State",
+                        "NUBASEMassExcess",
+                        "NUBASEMassExcessError",
+                        "IsomerEnergy",
+                        "IsomerEnergyError",
+                        "HalfLifeValue",
+                        "HalfLifeUnit",
+                        "HalfLifeError",
+                        "Spin",
+                        "DiscoveryYear",
+                        "DecayModes",
+                        ]
 
-    def _read_halflife_error(self, line: str) -> typing.Optional[float]:
-        """Slice the string to get the numerical value or None if it's empty."""
-        data = line[self.START_HALFLIFEERROR: self.END_HALFLIFEERROR].strip()
-        number = re.sub(r"[<>?~a-z]", "", data)
-        return float(number) if number else None
+    def _data_types(self) -> dict:
+        """Set the data type depending on the year"""
+        match self.year:
+            case 1995 | 2003:
+                return {
+                        "Symbol": "string",
+                        "A": "Int64",
+                        "Z": "Int64",
+                        "N": "Int64",
+                        "Experimental": "boolean",
+                        # "State": "Int64",
+                        "NUBASEMassExcess": "float64",
+                        "NUBASEMassExcessError": "float64",
+                        # "IsomerEnergy": "float64",
+                        # "IsomerEnergyError": "float64",
+                        "HalfLifeValue": "float64",
+                        "HalfLifeUnit": "string",
+                        "HalfLifeError": "float64",
+                        "Spin": "string",
+                        "DecayModes": "string",
+                        }
+            case _:
+                return {
+                        "Symbol": "string",
+                        "A": "Int64",
+                        "Z": "Int64",
+                        "N": "Int64",
+                        "Experimental": "boolean",
+                        # "State": "Int64",
+                        "NUBASEMassExcess": "float64",
+                        "NUBASEMassExcessError": "float64",
+                        # "IsomerEnergy": "float64",
+                        # "IsomerEnergyError": "float64",
+                        "HalfLifeValue": "float64",
+                        "HalfLifeUnit": "string",
+                        "HalfLifeError": "float64",
+                        "Spin": "string",
+                        "DiscoveryYear": "Int64",
+                        "DecayModes": "string",
+                        }
 
-    def _read_all_halflife_data(self, line: str) -> tuple:
-        """Extract all the data related to the halflife."""
-        data = line[self.START_HALFLIFEVALUE: self.END_HALFLIFEVALUE].strip()
-        number = re.sub(r"[<>?~]", "", data) if data != "stbl" else "-1.0"
-
-        if number == "-1.0":
-            return 99.99, "Zy", 0.0
-
-        return (
-            float(number) if number else None,
-            self._read_substring(line, self.START_HALFLIFEUNIT, self.END_HALFLIFEUNIT),
-            self._read_halflife_error(line)
-        )
-
-    def _read_spin(self, line: str) -> typing.Optional[str]:
-        """Extract the spin of the isotope and it's level."""
-        # 2020 brought in '*' for directly measured. Just remove it for the moment
-        # TODO parse the spin parity with the new characters
-        spin = self._read_substring(line, self.START_SPIN, self.END_SPIN)
-        if spin and spin.find('*') != -1:
-            spin = spin.replace('*', '')
-
-        return spin
-
-    def _read_decay_string(self, line: str) -> str:
-        """Extract the decay mode and do some book keeping for consistency."""
-        decay_string = (
-            self._read_substring(line, self.START_DECAYSTRING_03, len(line))
-            if self.year == 2003
-            else self._read_substring(line, self.START_DECAYSTRING, len(line))
-        )
-
-        if decay_string is None:
-            decay_string = "UNKNOWN"
-        # Get the first value in the ';' separated list
-        # Get the initial part of the first value
-        filtered = re.split('=| |~|<|>', decay_string.split(';')[0])[0]
-
-        # Tidy up 'random' strings into standard ones
-        if filtered == "e+":
-            filtered = "B+"
-        elif filtered == "IS":
-            filtered = "stable"
-
-        return filtered
-
-    def _read_line(self, line: str) -> dict:
-        """Read a line of the file."""
-        # Ignore isomers for the moment
-        state = self._read_as_int(line, self.START_STATE, self.END_STATE)
-        if state is None or state > 0:
-            return dict()
-
-        exp = True if line.find("#") == -1 else False
-        if not exp:
-            line = line.replace("#", " ")
-
-        data = {
-            "Experimental": exp,
-            "TableYear": self.year,
-            "A": self._read_as_int(line, self.START_A, self.END_A),
-            "Z": self._read_as_int(line, self.START_Z, self.END_Z),
-            "NUBASEMassExcess": self._read_as_float(line, self.START_ME, self.END_ME),
-            "NUBASEMassExcessError": self._read_as_float(line, self.START_DME, self.END_DME),
-            # "LevelEnergy" : self._read_as_float(,
-            #     line, self.START_ISOMER, self.END_ISOMER
-            # )
-            # "LevelEnergyError" : self._read_as_float(,
-            #     line, self.START_DISOMER, self.END_DISOMER
-            # )
-            "LevelSpin": self._read_spin(line),
-            "DiscoveryYear": (
-                self._read_as_int(line, self.START_YEAR, self.END_YEAR, default=1900)
-                if self.year != 2003
-                else 1900
-            ),
-            "Decay": self._read_decay_string(line),
-        }
-
-        data["HalfLifeValue"], data["HalfLifeUnit"], data["HalfLifeError"] = self._read_all_halflife_data(line)
-        data["N"] = data["A"] - data["Z"]
-        data["Symbol"] = self.z_to_symbol[data["Z"]]
-
-        return data
-
-    def _readable_line(self, line: str) -> bool:
-        """Some lines have 'random' strings, ignore them."""
-        return (line.find("p-unst") == -1 and line.find("mix") == -1 and line.find("non-exist") == -1)
+    def _na_values(self) -> dict:
+        """Set the columns that have placeholder values"""
+        match self.year:
+            case 1995:
+                return {
+                        "NUBASEMassExcess": [''],
+                        "NUBASEMassExcessError": [''],
+                        "State": [''],
+                        "HalfLifeValue": [''],
+                        "HalfLifeUnit": [''],
+                        "HalfLifeError": [''],
+                        "Spin": [''],
+                        "DecayModes": [''],
+                        }
+            case _:
+                return {
+                        "State": [''],
+                        "NUBASEMassExcess": [''],
+                        "NUBASEMassExcessError": [''],
+                        "HalfLifeValue": ['', 'p-unst', 'p-unst#'],
+                        "HalfLifeUnit": [''],
+                        "HalfLifeError": [''],
+                        "DiscoveryYear": [''],
+                        "DecayModes": [''],
+                        }
 
     def read_file(self) -> pd.DataFrame:
-        """Read the file."""
-        with open(self.filename, "r") as f:
-            lines = [line.rstrip() for line in f]
+        """Read the file using it's known format
 
-        lines = lines[self.HEADER:self.FOOTER]
+        The AMEMassFile and other functions in this class have hopefully sanitized the
+        column names, data types and locations of the date so we can not make the generic
+        call to parse the file.
+        """
+        try:
+            df = pd.read_fwf(
+                    self.filename,
+                    colspecs=self.column_limits,
+                    names=self._column_names(),
+                    na_values=self._na_values(),
+                    keep_default_na=False,
+                    on_bad_lines='warn',
+                    skiprows=self.HEADER,
+                    skipfooter=self.FOOTER
+                    )
+            # We use the NUBASE data to define whether or not an isotope is experimentally measured,
+            df['Experimental'] = ~df["NUBASEMassExcess"].astype("string").str.contains('#', na=False)
+            df.replace("#", "", regex=True, inplace=True)
 
-        the_lines = [
-            self._read_line(line) for line in lines if self._readable_line(line)
-        ]
+            df["N"] = pd.to_numeric(df["A"]) - pd.to_numeric(df["Z"])
+            df["Symbol"] = pd.to_numeric(df["Z"]).map(self.z_to_symbol)
+            # For the moment, we will ignore anything this is not the ground state
+            df = df[df["State"] == 0]
+            # As 'State' is now necessarily 0 and the Isomer columns are empty. Drop them.
+            df = df.drop(columns=['State', 'IsomerEnergy', 'IsomerEnergyError'])
 
-        return pd.DataFrame([d for d in the_lines if d])
+            # Convert stable isotopes into ones with enormous lifetimes with zero error so we can cast
+            df.loc[df['HalfLifeValue'] == 'stbl', ['HalfLifeValue', 'HalfLifeUnit', 'HalfLifeError']] = [99.99, 'Zy', 0.0]
+
+            df['HalfLifeValue'] = df['HalfLifeValue'].astype("string").str.replace(r'[<>?~]','', regex=True)
+            # We'll be lazy here an remove any characters in this column. Future us will parse this properly
+            df['HalfLifeError'] = df['HalfLifeError'].astype("string").str.replace(r'[<>?~a-z]','', regex=True)
+
+            return df.astype(self._data_types())
+        except ValueError as e:
+            print(f"Parsing error: {e}")
