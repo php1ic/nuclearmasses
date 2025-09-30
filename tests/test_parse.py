5a1647d9bf132637ed8cdc16f388c47dd591b1a1
Ian Cullen <ian.james.cullen@gmail.com>
2025-09-30 21:08:07 +0100

    Update tests to match ElementConverter class

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,34 +1,18 @@
-from nuclearmasses.parse import Parse
+from nuclearmasses.element_converter import ElementConverter
 
 
-def test_read_as_int():
-    Parser = Parse()
+def test_Z_to_symbol():
+    converter = ElementConverter()
 
-    line = "123456789"
-    assert Parser._read_as_int(line, 0, 2) == 12
-    assert Parser._read_as_int(line, 5, len(line)) == 6789
-    assert Parser._read_as_int(line, 3, 4) == 4
-
-    assert not Parser._read_as_int("", 3, 6)
-    assert Parser._read_as_int("  345    ", 0, len("  345    ")) == 345
+    assert converter.z_to_symbol[0] == "n"
+    assert converter.z_to_symbol[6] == "C"
+    assert converter.z_to_symbol[104] == "Rf"
 
 
-def test_read_as_float():
-    Parser = Parse()
+def test_symbol_to_Z():
+    converter = ElementConverter()
 
-    line = "1.23456789"
-    assert Parser._read_as_float(line, 0, 2) == 1.0
-    assert Parser._read_as_float(line, 0, 3) == 1.2
-    assert Parser._read_as_float(line, 0, 6) == 1.2345
+    assert converter.symbol_to_z["Al"] == 13
+    assert converter.symbol_to_z["Fe"] == 26
+    assert converter.symbol_to_z["Po"] == 84
 
-    assert not Parser._read_as_float("*", 0, len("*"))
-    assert not Parser._read_as_float("  *  ", 0, len("  *  "))
-
-
-def test_read_as_substring():
-    Parser = Parse()
-
-    line = "Some information in a string"
-    assert Parser._read_substring(line, 0, 4) == "Some"
-    assert not Parser._read_substring(line, 4, 5)
-    assert not Parser._read_substring(" ", 0, 5)
