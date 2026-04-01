31f5b6a68296190d97e33184b84730dfb1ad6a76
Ian Cullen <ian.james.cullen@gmail.com>
2026-04-01 21:00:21 +0100

    Update the MassTable class to make use of the new structure

--- src/nuclearmasses/mass_table.py
+++ src/nuclearmasses/mass_table.py
@@ -1,12 +1,9 @@
 import importlib.resources
-import pathlib
+import typing
 
 import pandas as pd
 
-from nuclearmasses.io.ame_mass_parse import AMEMassParser
-from nuclearmasses.io.ame_reaction_1_parse import AMEReactionParserOne
-from nuclearmasses.io.ame_reaction_2_parse import AMEReactionParserTwo
-
+from nuclearmasses.io.ame import AME
 from nuclearmasses.io.nubase import NUBASE
 
 
@@ -16,90 +13,83 @@ class MassTable:
     Internally there are separate dataframes for the NUBASE and AME data as well as a combined one for all data
     """
 
-    def __init__(self):
-        """Do all of the work at construction."""
-        self.data_path = importlib.resources.files("nuclearmasses.data")
-        self.nubase = NUBASE(self.data_path).nubase_df
-        self.ame_years = [1983, 1993, 1995, 2003, 2012, 2016, 2020]
-        self.ame = pd.concat([self._parse_ame_data(y) for y in self.ame_years], ignore_index=True)
-        self.full_data = self._combine_all_data()
-        self._do_indexing()
+    def __init__(self, df: pd.DataFrame | None = None, filters: list[tuple[str, str, typing.Any]] | None = None):
+        self._original_df: pd.DataFrame = self._parse_files() if df is None else df
+        self._filters: list[tuple[str, str, typing.Any]] = filters or []
+        self._create_dynamic_getters()
 
-    def _get_ame_datafiles(self, year: int) -> tuple[pathlib.Path, pathlib.Path, pathlib.Path]:
-        """Use the given year to locate the 3 AME data file and return the absolute paths."""
-        data_dir = self.data_path / pathlib.Path(str(year))
-        data_dir = data_dir.resolve()
+    def __repr__(self) -> str:
+        """Make printing the class object show the DataFrame nicely"""
+        return repr(self.df)
 
-        match year:
-            case 1983:
-                ame_mass = data_dir / "mass.mas83"
-                ame_reaction_1 = data_dir / "rct1.mas83"
-                ame_reaction_2 = data_dir / "rct2.mas83"
-            case 1993:
-                ame_mass = data_dir / "mass_exp.mas93"
-                ame_reaction_1 = data_dir / "rct1_exp.mas93"
-                ame_reaction_2 = data_dir / "rct2_exp.mas93"
-            case 1995:
-                ame_mass = data_dir / "mass_exp.mas95"
-                ame_reaction_1 = data_dir / "rct1_exp.mas95"
-                ame_reaction_2 = data_dir / "rct2_exp.mas95"
-            case 2003:
-                ame_mass = data_dir / "mass.mas03"
-                ame_reaction_1 = data_dir / "rct1.mas03"
-                ame_reaction_2 = data_dir / "rct2.mas03"
-            case 2012:
-                ame_mass = data_dir / "mass.mas12"
-                ame_reaction_1 = data_dir / "rct1.mas12"
-                ame_reaction_2 = data_dir / "rct2.mas12"
-            case 2016:
-                ame_mass = data_dir / "mass16.txt"
-                ame_reaction_1 = data_dir / "rct1-16.txt"
-                ame_reaction_2 = data_dir / "rct2-16.txt"
-            case 2020:
-                ame_mass = data_dir / "mass.mas20"
-                ame_reaction_1 = data_dir / "rct1.mas20"
-                ame_reaction_2 = data_dir / "rct2.mas20"
+    def __str__(self) -> str:
+        """Make printing the class object show the DataFrame nicely"""
+        return str(self.df)
 
-        return ame_mass, ame_reaction_1, ame_reaction_2
+    def __getattr__(self, attr: str) -> typing.Any:
+        """Delegate pandas methods for deeper chaining"""
+        if hasattr(self.df, attr):
+            return getattr(self.df, attr)
+        raise AttributeError(f"'{type(self).__name__}' object has no attribute '{attr}'")
 
-    def _parse_ame_data(self, year: int) -> pd.DataFrame:
-        """Combine all the AME files from the given year into a pandas.DataFrame."""
-        ame_mass, ame_reaction_1, ame_reaction_2 = self._get_ame_datafiles(year)
+    def __dir__(self):
+        """Pass the pandas api through so we can get autocomplete"""
+        return sorted(set(list(self.__dict__.keys()) + dir(type(self)) + dir(self.df)))
 
-        ame_mass_df = AMEMassParser(ame_mass, year).read_file()
+    # def __len__(self) -> int:
+    #     return len(self.df)
+    #
+    # def __iter__(self):
+    #     return iter(self.df)
+    #
+    # def __getitem__(self, key):
+    #     return type(self)(df=self.df[key])
 
-        # Merge all 3 of the AME files/data frames into one
+    def get(self, column: str, value: typing.Any) -> typing.Self:
+        """Generic getter: all_data.get('A', 123)"""
+        new_filters = self._filters + [(column, "==", value)]
+        return type(self)(df=self._original_df, filters=new_filters)
+
+    def filter(self, expr: str) -> typing.Self:
+        """Allow arbitrary pandas .query() expressions."""
+        # For simplicity we still store as tuples; you could store raw expressions too
+        new_filters = self._filters + [("query", expr, None)]
+        return type(self)(df=self._original_df, filters=new_filters)
+
+    def _parse_files(self) -> pd.DataFrame:
+        data_path = importlib.resources.files("nuclearmasses").joinpath("data")
+
         common_columns = ["A", "Z", "N", "TableYear", "Symbol"]
-        temp_df = ame_mass_df.merge(AMEReactionParserOne(ame_reaction_1, year).read_file(), on=common_columns)
-        return temp_df.merge(AMEReactionParserTwo(ame_reaction_2, year).read_file(), on=common_columns)
 
-    def _combine_all_data(self) -> pd.DataFrame:
-        """Combine all NUBASE and AME data into a single pandas DataFrame."""
-        common_columns = ["A", "Z", "N", "TableYear", "Symbol"]
-        df = pd.merge(self.ame, self.nubase, on=common_columns, how="outer")
+        return pd.merge(AME(data_path).ame_df, NUBASE(data_path).nubase_df, on=common_columns, how="outer")
 
-        df["NUBASERelativeError"] = abs(df["NUBASEMassExcessError"] / df["NUBASEMassExcess"])
-        df["AMERelativeError"] = abs(df["AMEMassExcessError"] / df["AMEMassExcess"])
+    @property
+    def df(self) -> pd.DataFrame:
+        """Apply all filters only when .df is accessed"""
+        result = self._original_df
+        for key, op, val in self._filters:
+            if op == "==":
+                # Filter on the index
+                if key == self._original_df.index.name:
+                    result = result[result.index == val]
+                # Filter on a regular column
+                else:
+                    result = result[result[key] == val]
+            elif key == "query":
+                result = result.query(op)
+        return result
 
-        # 12C has a 0.0 +/ 0.0 mass excess by definition so calculating relative error -> NaN
-        # Set the value to 0.0 as that's what it is
-        mask = (df.Symbol == "C") & (df.A == 12)
-        df.loc[mask, "NUBASERelativeError"] = 0.0
-        df.loc[mask, "AMERelativeError"] = 0.0
+    def _create_dynamic_getters(self):
+        """Automatically create get_colname(value) methods for every column."""
+        for col in self._original_df.columns:
+            method_name = f"get_{col}"
 
-        # 198Au has a typo in it's decay mode in the 2012 table. It is recorded as '-'
-        df.loc[(df.A == 198) & (df.Z == 79) & (df.TableYear == 2012), "DecayModes"] = "B-"
+            def make_getter(column: str):
+                def getter(self, value: typing.Any) -> MassTable:
+                    new_filters = self._filters + [(column, "==", value)]
+                    return type(self)(df=self._original_df, filters=new_filters)
 
-        return df
+                return getter
 
-    def _do_indexing(self) -> None:
-        """
-        Set the index of the dataframe to the table year. This is done in place so nothing is returned.
-
-        param: Nothing
-
-        :return: Nothing
-        """
-        self.nubase.set_index("TableYear", inplace=True)
-        self.ame.set_index("TableYear", inplace=True)
-        self.full_data.set_index("TableYear", inplace=True)
+            # Attach the method to the class/instance
+            setattr(self, method_name, make_getter(col).__get__(self, MassTable))
